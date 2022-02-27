93a20a325c49d450939465f71e5176f4c8c4ba68
Julius Gvozdiovas <J.Gvozdiovas@student.tudelft.nl>
2022-02-27 16:14:41 +0100

    Initial test setup

---
+++ server/src/test/java/server/api/QuestionControllerTest.java
@@ -0,0 +1,11 @@
+package server.api;
+
+import server.utils.RandomUtils;
+
+public class QuestionControllerTest {
+
+    private RandomUtils randomUtils;
+    private TestQuestionRepository repo;
+
+    private QuoteController ctrl;
+}
