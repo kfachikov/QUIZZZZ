2777e915e59c1ace85d01220527e748b406b5751
Kaloyan Fachikov <K.T.Fachikov@student.tudelft.nl>
2022-03-04 12:01:27 +0100

    Create Activity class - create according fields - generate according getters and setters - write JavaDoc for the present methods

---
+++ server/src/main/java/server/database/entities/Activity.java
@@ -0,0 +1,117 @@
+package server.database.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long row;
+
+    private String id;
+    private String title;
+    private String source;
+
+    @Column(name = "image_path")
+    private String image;
+
+    @Column(name = "consumption_in_wh")
+    private String consumption;
+
+    /**
+     * Get the primary-key attribute of an entity.
+     * @return The row attribute of a particular entry.
+     */
+    public Long getRow() {
+        return row;
+    }
+
+    /**
+     * Sets row field - primary key in table.
+     * @param row Long value to be set as row attribute.
+     */
+    public void setRow(Long row) {
+        this.row = row;
+    }
+
+    /**
+     * Get the identifying attribute, consisting of group number and the activity itself.
+     * @return The id attribute of a particular entry.
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Sets id field of an entry.
+     * @param id String value to be set as an id attribute.
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Get the description of an activity.
+     * @return The title attribute of a particular entry.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets title field of an entry.
+     * @param title String value to be set as a title attribute.
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Get the source, where the information for the particular activity has been gathered from.
+     * @return The source attribute of a particular entry.
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     * Sets source field of an entry.
+     * @param source String value to be set as a source attribute.
+     */
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    /**
+     * Get the image showing the activity "in action".
+     * @return The image attribute of a particular entry.
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * Sets image field of an entry.
+     * @param image String value to be set as an image attribute.
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    /**
+     * Get the consumption, stored in watt-hours of a particular activity.
+     * @return The title attribute of a particular entry.
+     */
+    public String getConsumption() {
+        return consumption;
+    }
+
+    /**
+     * Sets consumption field of an entry.
+     * @param consumption String value to be set as a consumption attribute.
+     */
+    public void setConsumption(String consumption) {
+        this.consumption = consumption;
+    }
+}
