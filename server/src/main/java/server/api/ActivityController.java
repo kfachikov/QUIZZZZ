945ae367529bc705fbf2a829ac5ad87a82df9698
kayra <K.Bahadr@student.tudelft.nl>
2022-03-04 17:12:09 +0100

    Eliminate checkstyle error

--- server/src/main/java/server/api/ActivityController.java
+++ server/src/main/java/server/api/ActivityController.java
@@ -1,6 +1,5 @@
 package server.api;
 
-import commons.MultiUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
