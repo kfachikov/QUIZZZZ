366d17c366d8faaa864867d07e42535d125a516b
Alexandra Darie <A.Darie@student.tudelft.nl>
2022-03-05 09:32:30 +0100

    I added some deleted code

--- commons/src/main/java/commons/BasicMultipleChoiceQuestion.java
+++ commons/src/main/java/commons/BasicMultipleChoiceQuestion.java
@@ -5,7 +5,10 @@ import javax.persistence.Entity;
 @Entity
 public class BasicMultipleChoiceQuestion extends AbstractQuestion {
 
-
+    @SuppressWarnings("unused")
+    private BasicMultipleChoiceQuestion() {
+        super();
+    }
 
     public BasicMultipleChoiceQuestion(String baseTitle, String imageFilename, long consumptionWh) {
         super(baseTitle, imageFilename, consumptionWh);
