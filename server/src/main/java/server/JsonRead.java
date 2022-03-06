e4d47fd52c3b49dccd3e4aa9847f50f5fcf5a807
kayra <K.Bahadr@student.tudelft.nl>
2022-03-06 14:10:20 +0100

    Add Activity constructor for object mapping

---
+++ server/src/main/java/server/JsonRead.java
@@ -0,0 +1,13 @@
+package server;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+//import server.database.entities.Activity;
+
+public class JsonRead {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //Activity activity = new Activity("id", "title", "source", "image", 100L);
+
+    }
+}
