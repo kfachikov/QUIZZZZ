33927aa54ca8a62aa5000132a34f6a3445eb28f3
Marijn Luime <m.d.luime@student.tudelft.nl>
2022-03-06 22:36:34 +0100

    Fixed API endpoint delete.
    Fixed API endpoint delete.

--- server/src/main/java/server/api/LobbyController.java
+++ server/src/main/java/server/api/LobbyController.java
@@ -38,17 +38,18 @@ public class LobbyController {
     }
 
 
-    /**
-     * @param id is the id of the user.
-     * @return returns a ResponseEntity.
+    /** Delete a user if present from the repositroy.
+     * @param id Primary-key attribute to search with
+     * @return returns a ResponseEntity consisting of the deleted user if present or a Not Found error if not found.
      */
-    @DeleteMapping("")
+    @DeleteMapping("/{id}")
     public ResponseEntity<MultiUser> deleteUser(@PathVariable("id") long id) {
+        MultiUser removed = repo.getById(id);
         if (id < 0 || !repo.existsById(id)) {
             return ResponseEntity.badRequest().build();
         }
         repo.delete(repo.getById(id));
-        return ResponseEntity.ok(repo.getById(id));
+        return ResponseEntity.ok(removed);
     }
 
 }
