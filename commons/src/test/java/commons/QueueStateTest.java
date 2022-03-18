02ce81c3cf44215f6d188b1aa80f59f8b8320313
kayra <K.Bahadr@student.tudelft.nl>
2022-03-18 11:56:44 +0100

    Refactor QueueStateTest

--- commons/src/test/java/commons/QueueStateTest.java
+++ commons/src/test/java/commons/QueueStateTest.java
@@ -2,146 +2,145 @@ package commons;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueStateTest {
+    private QueueUser user1;
+    private QueueUser user2;
+    private List<QueueUser> users;
 
-    private QueueState emptyState1;
-    private QueueState emptyState2;
-    private QueueState emptyState3;
-
-    private QueueState stateA1;
-    private QueueState stateA2;
-
-    private QueueState stateB1;
-    private QueueState stateB2;
-
-    private QueueState stateC1;
-    private QueueState stateC2;
-    private QueueState stateC3;
+    private boolean gameStarting;
+    private long msToStart;
+    private long upcomingGameId;
 
     @BeforeEach
     void setUp() {
-        // Initialize empty QueueStates
-        this.emptyState1 = new QueueState(new ArrayList<>());
-        this.emptyState2 = new QueueState();
-        this.emptyState3 = new QueueState(new ArrayList<>(), false, 3000);
-
-        // Initialize 1st set of example QueueStates
-        QueueUser queueUser1 = new QueueUser("TestUser1");
-        QueueUser queueUser2 = new QueueUser("TestUser2");
-        QueueUser queueUser3 = new QueueUser("TestUser3");
-
-        // QueueStates are initialized using the same QueueUser instances
-        this.stateA1 = new QueueState(Arrays.asList(queueUser1, queueUser2, queueUser3));
-        this.stateA2 = new QueueState(Arrays.asList(queueUser1, queueUser2, queueUser3));
-
-        // Initialize 2nd set of example QueueStates
-        // They are initialized using different QueueUser instances
-        this.stateB1 = new QueueState(Arrays.asList(
-                new QueueUser("TestUser4"),
-                new QueueUser("TestUser5"),
-                new QueueUser("TestUser6")
-        ));
-        this.stateB2 = new QueueState(Arrays.asList(
-                new QueueUser("TestUser4"),
-                new QueueUser("TestUser5"),
-                new QueueUser("TestUser6")
-        ));
-        // QueueState with different order
-        this.stateC1 = new QueueState(Arrays.asList(
-                new QueueUser("TestUser4"),
-                new QueueUser("TestUser6"),
-                new QueueUser("TestUser5")
-        ));
-        // QueueState that is starting
-        this.stateC2 = new QueueState(Arrays.asList(
-                new QueueUser("TestUser4"),
-                new QueueUser("TestUser5"),
-                new QueueUser("TestUser6")
-        ), true, 3000);
-        // QueueState whose countdown is ticking down
-        this.stateC3 = new QueueState(Arrays.asList(
-                new QueueUser("TestUser4"),
-                new QueueUser("TestUser5"),
-                new QueueUser("TestUser6")
-        ), true, 1500);
+        gameStarting = true;
+        msToStart = 10L;
+        upcomingGameId = 10L;
+        users = new ArrayList<>();
+        user1 = new QueueUser("username1");
+        user2 = new QueueUser("username2");
+        users.add(user1);
+        users.add(user2);
     }
 
     @Test
-    public void checkConstructorEmpty() {
-        assertNotNull(emptyState2.users);
+    void getUsers() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        assertEquals(users, queueState.getUsers());
     }
 
     @Test
-    public void checkConstructorNonEmpty() {
-        assertNotNull(stateA1.users);
-        assertEquals(3, stateA1.users.size());
+    void setUsers() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        List<QueueUser> newUsers = new ArrayList<>();
+        newUsers.add(user1);
+        queueState.setUsers(newUsers);
+        assertEquals(newUsers, queueState.getUsers());
+
     }
 
     @Test
-    public void equalsHashCode1() {
-        assertEquals(stateA1, stateA2);
-        assertEquals(stateA1.hashCode(), stateA2.hashCode());
+    void isGameStarting() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        assertEquals(gameStarting, queueState.isGameStarting());
     }
 
     @Test
-    public void equalsHashCode2() {
-        assertEquals(stateB1, stateB2);
-        assertEquals(stateB1.hashCode(), stateB2.hashCode());
+    void setGameStarting() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        boolean newGameStarting = false;
+        queueState.setGameStarting(newGameStarting);
+        assertEquals(false, queueState.isGameStarting());
     }
 
     @Test
-    public void equalsHashCodeEmpty() {
-        assertEquals(emptyState1, emptyState2);
-        assertEquals(emptyState1, emptyState3);
-        assertEquals(emptyState3, emptyState2);
-        assertEquals(emptyState1.hashCode(), emptyState2.hashCode());
-        assertEquals(emptyState1.hashCode(), emptyState3.hashCode());
-        assertEquals(emptyState3.hashCode(), emptyState2.hashCode());
+    void getMsToStart() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        assertEquals(msToStart, queueState.getMsToStart());
     }
 
     @Test
-    public void notEqualsHashCodeDifferent() {
-        assertNotEquals(stateA1, stateB1);
-        assertNotEquals(stateA1.hashCode(), stateB1.hashCode());
+    void setMsToStart() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        long newMsToStart = 1L;
+        queueState.setMsToStart(newMsToStart);
+        assertEquals(1L, queueState.getMsToStart());
     }
 
     @Test
-    public void notEqualsHashCodeShuffled() {
-        // Order matters
-        assertNotEquals(stateC1, stateB1);
-        assertNotEquals(stateC1.hashCode(), stateB1.hashCode());
+    void getUpcomingGameId() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        assertEquals(upcomingGameId, queueState.getUpcomingGameId());
     }
 
     @Test
-    public void notEqualsHashCodeStarted() {
-        // gameStarted matters
-        assertNotEquals(stateC2, stateB1);
-        assertNotEquals(stateC2.hashCode(), stateB1.hashCode());
+    void setUpcomingGameId() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        long newUpcomingGameId = 1L;
+        queueState.setUpcomingGameId(newUpcomingGameId);
+        assertEquals(1L, queueState.getUpcomingGameId());
     }
 
     @Test
-    public void notEqualsHashCodeCountdown() {
-        // msToStart matters
-        assertNotEquals(stateC3, stateB1);
-        assertNotEquals(stateC3.hashCode(), stateB1.hashCode());
+    void testEquals() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        QueueState queueState2 = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        assertEquals(queueState, queueState2);
     }
 
     @Test
-    public void hasToString() {
-        var actual = stateA1.toString();
-        assertTrue(actual.contains(QueueState.class.getSimpleName()));
-        assertTrue(actual.contains(QueueUser.class.getSimpleName()));
-        assertTrue(actual.contains("\n"));
-        assertTrue(actual.contains("TestUser1"));
-        assertTrue(actual.contains("TestUser2"));
-        assertTrue(actual.contains("TestUser3"));
-        assertTrue(actual.contains("username"));
-        assertTrue(actual.contains("users"));
+    void testEqualsDifferentUsers() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        List<QueueUser> newUsers = new ArrayList<>();
+        newUsers.add(user1);
+        QueueState queueState2 = new QueueState(newUsers, gameStarting, msToStart, upcomingGameId);
+        assertNotEquals(queueState, queueState2);
+    }
+
+    @Test
+    void testEqualsDifferentGameStarting() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        boolean newGameStarting = false;
+        QueueState queueState2 = new QueueState(users, newGameStarting, msToStart, upcomingGameId);
+        assertNotEquals(queueState, queueState2);
+    }
+
+    @Test
+    void testEqualsDifferentMsToStart() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        QueueState queueState2 = new QueueState(users, gameStarting, 1L, upcomingGameId);
+        assertNotEquals(queueState, queueState2);
+    }
+
+    @Test
+    void testEqualsDifferentUpcomingGameId() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        QueueState queueState2 = new QueueState(users, gameStarting, msToStart, 1L);
+        assertNotEquals(queueState, queueState2);
+    }
+
+    @Test
+    void testHashCode() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        QueueState queueState2 = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        assertEquals(queueState.hashCode(), queueState2.hashCode());
+    }
+
+    @Test
+    void testHashCodeDifferent() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+        QueueState queueState2 = new QueueState(users, gameStarting, msToStart, 1L);
+        assertNotEquals(queueState.hashCode(), queueState2.hashCode());
+    }
+
+    @Test
+    void testToString() {
+        QueueState queueState = new QueueState(users, gameStarting, msToStart, upcomingGameId);
+
     }
 }
