34025b912adc8861102a8bd95580b577f4086c54
Julius Gvozdiovas <J.Gvozdiovas@student.tudelft.nl>
2022-03-24 14:55:29 +0100

    Add some basic state transitions

--- server/src/main/java/server/utils/MultiPlayerStateUtils.java
+++ server/src/main/java/server/utils/MultiPlayerStateUtils.java
@@ -41,6 +41,75 @@ public class MultiPlayerStateUtils {
     }
 
     /**
+     * Check and update (if needed) the state of the game.
+     *
+     * @param game Multiplayer game whose state is to be checked and updated.
+     */
+    public void updateState(MultiPlayerState game) {
+        if (checkIfUpdate(game)) {
+            switchState(game);
+        }
+    }
+
+    /**
+     * Check if state of the game has to be updated.
+     *
+     * @param game Game to be checked
+     * @return true iff current time is beyond time of update for the state of the game.
+     */
+    public boolean checkIfUpdate(MultiPlayerState game) {
+        return game.getNextPhase() <= new Date().getTime();
+    }
+
+    /**
+     * Switch the state of the game to the next appropriate state.
+     * <p>
+     * Beware of calling this method if the state should not be updated yet.
+     * It will prematurely switch the state of the game, and leave the next phase longer.
+     *
+     * @param game Game whose state is to be switched to the next one.
+     */
+    public void switchState(MultiPlayerState game) {
+        // Only update state if needed
+        String state = game.getState();
+        if (MultiPlayerState.NOT_STARTED_STATE.equals(state)) {
+            // All game states are started when put into `games`, so
+            // if a state somehow has not started yet, an error has occurred.
+            //
+            // However, there is not really a good way to resolve the error,
+            // so we just log it.
+            System.err.println("Attempted to update state of game that hasn't started yet.");
+        } else if (MultiPlayerState.STARTING_STATE.equals(state)) {
+            // We should definitely switch to a question phase.
+            startNextRound(game);
+        } else if (MultiPlayerState.QUESTION_STATE.equals(state)) {
+
+        }
+    }
+
+    /**
+     * Start the next round of the game.
+     * <p>
+     * This is one of the state transitions of the game:
+     * - STARTING -> QUESTION
+     * - TRANSITION -> QUESTION
+     * - LEADERBOARD -> QUESTION
+     * <p>
+     * The next state after calling this method is guaranteed to be QUESTION.
+     *
+     * @param game Game whose state is updated to QUESTION.
+     */
+    private void startNextRound(MultiPlayerState game) {
+        int currentRound = game.getRoundNumber();
+        long nextPhase = game.getNextPhase();
+
+        game.setRoundNumber(currentRound + 1);
+        game.setState(MultiPlayerState.QUESTION_STATE);
+        // 8 seconds for questions
+        game.setNextPhase(nextPhase + 8000);
+    }
+
+    /**
      * Start a new game.
      * <p>
      * This method is called whenever someone clicks "Go!" in the queue.
