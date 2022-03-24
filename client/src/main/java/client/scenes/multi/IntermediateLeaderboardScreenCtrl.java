fc4b9f4e06343f5e2c1d314f4fe4622b526f1fa5
kayra <K.Bahadr@student.tudelft.nl>
2022-03-24 15:35:37 +0100

    Create screen and controller

---
+++ client/src/main/java/client/scenes/multi/IntermediateLeaderboardScreenCtrl.java
@@ -0,0 +1,24 @@
+package client.scenes.multi;
+
+import client.scenes.misc.MainCtrl;
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+
+public class IntermediateLeaderboardScreenCtrl {
+    private ServerUtils server;
+    private MainCtrl mainCtrl;
+
+    /**
+     * initializes IntermediateLeaderboardScreenCtrl by connecting it to backend and frontend mainCtrl.
+     *
+     * @param server   is the server variable
+     * @param mainCtrl is the main controller variable
+     */
+    @Inject
+    public IntermediateLeaderboardScreenCtrl(ServerUtils server, MainCtrl mainCtrl) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+    }
+
+
+}
