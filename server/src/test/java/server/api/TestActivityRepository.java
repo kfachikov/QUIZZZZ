cc3c6d78fa8ec452fa65ea124c1d672b33a67724
Alexandra Darie <A.Darie@student.tudelft.nl>
2022-03-25 22:01:18 +0100

    Fix pipeline failure.

--- server/src/test/java/server/api/TestActivityRepository.java
+++ server/src/test/java/server/api/TestActivityRepository.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 class TestActivityRepository implements ActivityRepository {
 
