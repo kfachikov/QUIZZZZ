3cd1bca943e67416a8fa7299b87ecfdf5518fa43
kayra <K.Bahadr@student.tudelft.nl>
2022-03-31 23:26:46 +0200

    Fully implement help screen + functionality

--- client/src/main/java/client/scenes/misc/HelpScreenCtrl.java
+++ client/src/main/java/client/scenes/misc/HelpScreenCtrl.java
@@ -4,8 +4,10 @@ import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
+import org.checkerframework.checker.units.qual.C;
 
 /**
  *
@@ -30,6 +32,15 @@ public class HelpScreenCtrl {
     @FXML
     private Text title;
 
+    @FXML
+    private Text text1;
+
+    @FXML
+    private Text text2;
+
+    @FXML
+    private Text text3;
+
     /**
      * initializes HelpScreenCtrl by connecting it to backend and frontend mainCtrl.
      *
@@ -43,46 +54,62 @@ public class HelpScreenCtrl {
     }
 
     /**
-     * Sets the help screen to tab-2.
-     */
-    public void helpOne() {
-        //button actions
-        goBack.setOnAction(event -> mainCtrl.showHome());
-        next.setOnAction(event -> helpTwo());
-        //circle colors
-        //circle2.setStyle();
-        //title+text
-    }
-
-    /**
-     * Sets the help screen to tab-2.
-     */
-    public void helpTwo() {
-        //set screen 2
-        title.setText("MULTIPLAYER");
-        goBack.setOnAction(event -> helpOne());
-        next.setOnAction(event -> helpThree());
-        //circle colors
-        //title+text
-    }
-
-    /**
-     * Sets the help screen to tab-3.
-     */
-    public void helpThree() {
-        title.setText("JOKER CARDS");
-        goBack.setOnAction(event -> helpTwo());
-        next.setText("Return Home");
-        next.setOnAction(event -> mainCtrl.showHome());
-        helpOne();
-        //circle colors
-        //title+text
-    }
-
-    /**
      * sets title and scene to Home.
      */
     public void returnHome() {
         mainCtrl.showHome();
+    }
+
+    public void setScene1() {
+        //set texts
+        title.setText("SOLO");
+        text1.setText("Do you want to test your knowledge on daily energy consumptions?");
+        text2.setText("Play a SOLO game and beat the best score to display your username on the top of the all time leaderboard!");
+        text2.setLayoutY(346.0);
+        text3.setText("Answer with speed and accuracy to get the most points! Each question has only 8 seconds!");
+        //set circles
+        circle2.setFill(Paint.valueOf("ffffff"));
+        circle3.setFill(Paint.valueOf("ffffff"));
+        //set buttons
+        goBack.setOnAction(event -> mainCtrl.showHome());
+        next.setText("NEXT");
+        next.setOnAction(event -> setScene2());
+    }
+
+    public void setScene2() {
+        //set texts
+        title.setText("MULTIPLAYER");
+        text1.setText("Do you want to compare your knowledge to your friends?");
+        text2.setText("Join a lobby altogether and see who is the most knowledgeable!");
+        text2.setLayoutY(346.0);
+        text3.setText("Send instantaneous reactions during the game for everyone to see! User jokers for extra fun!");
+        //set circles
+        circle2.setFill(Paint.valueOf("b50000"));
+        circle3.setFill(Paint.valueOf("ffffff"));
+        //set buttons
+        goBack.setOnAction(event -> setScene1());
+        next.setText("NEXT");
+        next.setOnAction(event -> setScene3());
+    }
+
+    public void setScene3() {
+        //set texts
+        title.setText("JOKER CARDS");
+        text1.setText("DOUBLE POINTS — Double the points for that question. (Use before time runs out)");
+        text2.setText("REMOVE INCORRECT — Remove an incorrect answer from answer choices. (Disabled in guess questions)");
+        text2.setLayoutY(364.0);
+        text3.setText("SHORTEN TIME — Put opponents into rush by decreasing their question time.");
+        //set circles
+        circle2.setFill(Paint.valueOf("b50000"));
+        circle3.setFill(Paint.valueOf("b50000"));
+        //set buttons
+        goBack.setOnAction(event -> setScene2());
+        next.setText("HOME");
+        next.setOnAction(event -> {
+            //set scene as home
+            mainCtrl.showHome();
+            //prep home screen to be ready as default
+            setScene1();
+        });
     }
 }
