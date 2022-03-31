d3f3fc99f488b987e789ad73911ca590cdd7a8aa
kayra <K.Bahadr@student.tudelft.nl>
2022-03-31 11:27:42 +0200

    Partial redesign of help screen

--- client/src/main/java/client/scenes/misc/HelpScreenCtrl.java
+++ client/src/main/java/client/scenes/misc/HelpScreenCtrl.java
@@ -2,6 +2,10 @@ package client.scenes.misc;
 
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 /**
  *
@@ -10,6 +14,21 @@ public class HelpScreenCtrl {
 
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
+
+    @FXML
+    private Circle circle2;
+
+    @FXML
+    private Circle circle3;
+
+    @FXML
+    private Button next;
+
+    @FXML
+    private Button goBack;
+
+    @FXML
+    private Text title;
 
     /**
      * initializes HelpScreenCtrl by connecting it to backend and frontend mainCtrl.
@@ -21,6 +40,43 @@ public class HelpScreenCtrl {
     public HelpScreenCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
         this.server = server;
+    }
+
+    /**
+     * Sets the help screen to tab-2.
+     */
+    public void helpOne() {
+        //button actions
+        goBack.setOnAction(event -> mainCtrl.showHome());
+        next.setOnAction(event -> helpTwo());
+        //circle colors
+        circle2.setStyle();
+        //title+text
+    }
+
+    /**
+     * Sets the help screen to tab-2.
+     */
+    public void helpTwo() {
+        //set screen 2
+        title.setText("MULTIPLAYER");
+        goBack.setOnAction(event -> helpOne());
+        next.setOnAction(event -> helpThree());
+        //circle colors
+        //title+text
+    }
+
+    /**
+     * Sets the help screen to tab-3.
+     */
+    public void helpThree() {
+        title.setText("JOKER CARDS");
+        goBack.setOnAction(event -> helpTwo());
+        next.setText("Return Home");
+        next.setOnAction(event -> mainCtrl.showHome());
+        helpOne();
+        //circle colors
+        //title+text
     }
 
     /**
