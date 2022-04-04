8f9ec290e58c14073737c32b01aeef5e0a98437e
Monica-Alexandra Păun <m.a.paun@student.tudelft.nl>
2022-04-04 21:03:41 +0200

    Create method that color the button with the correct answer into green

--- client/src/main/java/client/scenes/single/question/ConsumptionQuestionScreenCtrl.java
+++ client/src/main/java/client/scenes/single/question/ConsumptionQuestionScreenCtrl.java
@@ -206,4 +206,18 @@ public class ConsumptionQuestionScreenCtrl extends QuestionScreen {
         return time;
     }
 
+    /**
+     * Change the color of the button that has the correct answer into green.
+     * The user will be able in this way to gain information during this game.
+     */
+    public void showCorrectAnswer() {
+        if (firstAnswer.getText().equals(question.getCorrectAnswer() + "Wh"))
+            firstAnswer.setStyle("-fx-background-color: #" + (Paint.valueOf("32cd32")).toString().substring(2));
+        if (secondAnswer.getText().equals(question.getCorrectAnswer() + "Wh"))
+            secondAnswer.setStyle("-fx-background-color: #" + (Paint.valueOf("32cd32")).toString().substring(2));
+        if (thirdAnswer.getText().equals(question.getCorrectAnswer() + "Wh"))
+            thirdAnswer.setStyle("-fx-background-color: #" + (Paint.valueOf("32cd32")).toString().substring(2));
+
+    }
+
 }
