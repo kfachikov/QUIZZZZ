06588267dfdabd965d7d0179eb75685f1efcb8e6
kayra <K.Bahadr@student.tudelft.nl>
2022-04-07 12:21:29 +0200

    Fix checkstyle

--- client/src/main/java/client/scenes/single/PrepScreenCtrl.java
+++ client/src/main/java/client/scenes/single/PrepScreenCtrl.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.TextAlignment;
 
 import java.util.ArrayList;
 import java.util.Collections;
